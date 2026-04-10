c31ed68c7f3f89eadadbd9dd408c3309ef31f95f
Abhishek <abhishekpant2024@gmail.com>
2026-04-10 11:57:45 +0530

    seperating frontend and backend code

--- client/src/api/dashboard.js
+++
@@ -1,7 +0,0 @@
-import api from "./axiosConfig";
-
-const fetchDashboardData = async () => {
-	return await api.get("/dashboard/get");
-};
-
-export { fetchDashboardData };
--- client/src/components/Loading.jsx
+++
@@ -1,5 +0,0 @@
-const Loading = ({className}) => {
-	return <span className={`loading loading-infinity loading-lg ${className}`}></span>;
-};
-
-export default Loading;
